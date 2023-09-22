fa9277a2909cfcc93cba29c05518a97bf30f24d4
Jacob Dietz <jdietz981@gmail.com>
2023-09-21 20:29:50 -0500

    fixed final console.logs for examples

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -13,9 +13,9 @@ function isSorted(arr) {
 }
 
 console.log(isSorted([1, 2, 3, 4, 5])); // true
-isSorted([1, 2, 4, 3, 5]); // false
-isSorted([2, 4, 6, 7, 8]); // true
-isSorted([5, 4, 3, 2, 1]); // false
+console.log(isSorted([1, 2, 4, 3, 5])); // false
+console.log(isSorted([2, 4, 6, 7, 8])); // true
+console.log(isSorted([5, 4, 3, 2, 1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
